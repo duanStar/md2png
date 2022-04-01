52c1127ae1959815a3ab9cba916c4a9ed60e26a9
Hongfei Duan <1715106673@qq.com>
2022-04-01 20:50:43 +0800

    fix: test

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
-const md2Png = require('..')
+const md2Png = require("..");
 
 // TODO: Implement module test
-test('md2png', () => {
+test("md2png", () => {
   expect(
-    md2Png('./README.md', { output: './dist/README.png', width: 500 })
-  ).toBeTruthy()
-})
+    md2Png("./README.md", { output: "/dist/README.png", width: 500 })
+  ).toBeTruthy();
+});
