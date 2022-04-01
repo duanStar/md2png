d5cf5370f53956d178c30ecbe9652a214eab92ab
Hongfei Duan <1715106673@qq.com>
2022-04-01 20:41:50 +0800

    feature: :sparkles:finish

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const md2Png = require('..')
 
 // TODO: Implement module test
 test('md2png', () => {
-  expect(md2Png('w')).toBe('w@zce.me')
-  expect(md2Png('w', { host: 'wedn.net' })).toBe('w@wedn.net')
-  expect(() => md2Png(100)).toThrow('Expected a string, got number')
+  expect(
+    md2Png('./README.md', { output: './dist/README.png', width: 500 })
+  ).toBeTruthy()
 })
