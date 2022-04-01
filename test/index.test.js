c9e71960a0f446210e2c5122a515454b40e63f46
Hongfei Duan <1715106673@qq.com>
2022-04-01 20:55:57 +0800

    ..

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ const md2Png = require("..");
 // TODO: Implement module test
 test("md2png", () => {
   expect(
-    md2Png("./README.md", { output: "/dist/README.png", width: 500 })
+    md2Png("./README.md", { output: "./dist/README.png", width: 500 })
   ).toBeTruthy();
 });
